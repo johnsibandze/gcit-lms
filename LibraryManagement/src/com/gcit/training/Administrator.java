a17bdc364682effb5c7b287f4370b5448e24fbe0
johnsibandze <johnsibandze@lclark.edu>
2015-06-28 16:00:07 -0400

    Can Add Publisher

--- LibraryManagement/src/com/gcit/training/Administrator.java
+++ LibraryManagement/src/com/gcit/training/Administrator.java
@@ -16,6 +16,8 @@ public class Administrator extends User {
 	private Book book;
 	private LibraryBranch libraryBranch;
 	private ArrayList<Author> authors;
+	private Publisher publisher;
+	private ResultSet rs;
 
 	private Scanner sc;
 
@@ -57,6 +59,7 @@ public class Administrator extends User {
 			handleBookAndAuthor();
 		} else if (decision == 2) {
 			// TODO handle publishers
+			handlePublisher();
 		} else if (decision == 3) {
 			// TODO handle library branches
 		} else if (decision == 4) {
@@ -67,6 +70,97 @@ public class Administrator extends User {
 			this.startMainMenu = true;
 		}
 
+	}
+
+	private void handlePublisher() {
+		System.out.println("1) Add Publisher");
+		System.out.println("2) Update Publisher");
+		System.out.println("3) Delete Publisher");
+		System.out.println("4) Quit to previous");
+
+		int choice = Integer.parseInt(sc.nextLine());
+
+		if (choice == 1) {
+			addPublisher();
+		} else if (choice == 2) {
+			// TODO update publisher
+		} else if (choice == 3) {
+			// TODO delete publisher
+		} else {
+			mainMenu();
+			return;
+		}
+
+	}
+
+	private void addPublisher() {
+		// int publisherChoice = displayPublishers();
+		// updatePublisherInfo(publisherChoice);
+
+		publisher = new Publisher();
+
+		System.out.println("Please Enter Publisher Name");
+		String publisherName = sc.nextLine();
+
+		System.out.println("Please Enter Publisher Address");
+		String publisherAddress = sc.nextLine();
+
+		//TODO validate phone number
+		System.out.println("Please Enter Publisher Phone number");
+		String publisherPhone = sc.nextLine();
+
+		publisher = new Publisher();
+		publisher.setPublisherName(publisherName);
+		publisher.setPublisherAddress(publisherAddress);
+		publisher.setPublisherPhone(publisherPhone);
+
+		insertPublisherToDatabase();
+
+		System.out.println("Successfully Added Publisher");
+	}
+
+	private void insertPublisherToDatabase() {
+		try {
+			String selectQuery = "insert into tbl_publisher (publisherName, publisherAddress, publisherPhone) values (?, ?, ?)";
+
+			PreparedStatement pstmt = conn.prepareStatement(selectQuery);
+			pstmt.setString(1, publisher.getPublisherName());
+			pstmt.setString(2, publisher.getPublisherAddress());
+			pstmt.setString(3, publisher.getPublisherPhone());
+
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private int displayPublishers() {
+		int publisherCounter = 0;
+		try {
+			String selectQuery = "select * from tbl_publisher";
+
+			PreparedStatement pstmt = conn.prepareStatement(selectQuery);
+
+			rs = pstmt.executeQuery();
+
+			publisherCounter = 1;
+			while (rs.next()) {
+				String publisherName = rs.getString("publisherName");
+				String publisherAddress = rs.getString("publisherAddress");
+
+				System.out.println(publisherCounter + ") " + publisherName
+						+ ", " + publisherAddress);
+				publisherCounter++;
+			}
+
+			System.out.println(publisherCounter + ") Quit to previous");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return publisherCounter;
 	}
 
 	private void handleBookAndAuthor() {
@@ -82,11 +176,13 @@ public class Administrator extends User {
 		} else if (choice == 2) {
 			updateBookAndAuthor();
 		} else if (choice == 3) {
-			// TODO delete book and author
 			deleteBookAndAuthor();
 		} else {
 			mainMenu();
+			return;
 		}
+
+		mainMenu();
 
 	}
 
@@ -98,56 +194,7 @@ public class Administrator extends User {
 		findAuthors();
 
 		deleteBookFromDatabase();
-	}
-
-	private void deleteBookFromDatabase() {
-		try {
-			// delete book from book authors table
-			String deleteQuery = "delete from tbl_book_authors where bookId=?";
-			PreparedStatement pstmt = conn.prepareStatement(deleteQuery);
-			pstmt.setInt(1, book.getBookId());
-			pstmt.executeUpdate();
-
-			// delete book from book table
-			deleteQuery = "delete from tbl_book where bookId=?";
-			pstmt = conn.prepareStatement(deleteQuery);
-			pstmt.setInt(1, book.getBookId());
-			pstmt.executeUpdate();
-
-			// delete book from book copies table
-			deleteQuery = "delete from tbl_book_copies where bookId=?";
-			pstmt = conn.prepareStatement(deleteQuery);
-			pstmt.setInt(1, book.getBookId());
-			pstmt.executeUpdate();
-
-			// only delete authors if they don't have other books
-			deleteAuthors();
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	private void deleteAuthors() {
-		for (Author author : authors) {
-			if (!author.hasBooks()) {
-				deleteAuthor(author);
-			}
-		}
-	}
-
-	private void deleteAuthor(Author author) {
-		try {
-			String deleteQuery = "delete from tbl_author where authorId=?";
-			PreparedStatement pstmt = conn.prepareStatement(deleteQuery);
-			pstmt.setInt(1, author.getAuthorId());
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+		System.out.println("sucessfully deleted book!");
 	}
 
 	private void updateBookAndAuthor() {
@@ -157,84 +204,7 @@ public class Administrator extends User {
 		selectBook();
 
 		updateBookInDatabase();
-	}
-
-	private void findAuthors() {
-		try {
-			String selectQuery = "SELECT * FROM (tbl_author NATURAL JOIN tbl_book_authors) where bookId=?";
-
-			PreparedStatement pstmt = conn.prepareStatement(selectQuery);
-			pstmt.setInt(1, book.getBookId());
-
-			ResultSet rs = pstmt.executeQuery();
-
-			while (rs.next()) {
-				int authorId = rs.getInt("authorId");
-				String authorName = rs.getString("authorName");
-
-				Author author = new Author(authorId, authorName);
-
-				authors.add(author);
-
-			}
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	private void updateBookInDatabase() {
-		System.out.println("enter new book title");
-		String title = sc.nextLine();
-
-		System.out.println("enter author name");
-		String authorName = sc.nextLine();
-
-		try {
-			String updateQuery = "update ((tbl_book natural join tbl_book_authors) natural join tbl_author) set title=?, authorName=? where bookId=?";
-
-			PreparedStatement pstmt = conn.prepareStatement(updateQuery);
-
-			pstmt.setString(1, title);
-			pstmt.setString(2, authorName);
-			pstmt.setInt(3, book.getBookId());
-
-			pstmt.executeUpdate();
-
-			System.out.println("sucessfully updated!");
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	private void selectBook() {
-		int bookCounter = displayBooks(libraryBranch.getBranchId());
-
-		int bookChoice = Integer.parseInt(sc.nextLine());
-
-		if (bookCounter == bookChoice) {
-			handleBookAndAuthor();
-			return;
-		}
-
-		updateBookInfo(bookChoice);
-	}
-
-	private void selectLibrary() {
-		int libraryCounter = displayLibraries();
-
-		int libraryChoice = Integer.parseInt(sc.nextLine());
-
-		if (libraryChoice == libraryCounter) {
-			handleBookAndAuthor();
-			return;
-		}
-
-		updateLibraryInfo(libraryChoice);
-
+		System.out.println("sucessfully updated book!");
 	}
 
 	private void addBookAndAuthor() {
@@ -289,6 +259,133 @@ public class Administrator extends User {
 
 		// add copies of this book to the chosen library branch
 		addToBookCopies();
+		System.out.println("sucessfully added book!");
+	}
+
+	private void deleteBookFromDatabase() {
+		try {
+			// delete book from book authors table
+			String deleteQuery = "delete from tbl_book_authors where bookId=?";
+			PreparedStatement pstmt = conn.prepareStatement(deleteQuery);
+			pstmt.setInt(1, book.getBookId());
+			pstmt.executeUpdate();
+
+			// delete book from book table
+			deleteQuery = "delete from tbl_book where bookId=?";
+			pstmt = conn.prepareStatement(deleteQuery);
+			pstmt.setInt(1, book.getBookId());
+			pstmt.executeUpdate();
+
+			// delete book from book copies table
+			deleteQuery = "delete from tbl_book_copies where bookId=?";
+			pstmt = conn.prepareStatement(deleteQuery);
+			pstmt.setInt(1, book.getBookId());
+			pstmt.executeUpdate();
+
+			// only delete authors if they don't have other books
+			deleteAuthors();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void deleteAuthors() {
+		for (Author author : authors) {
+			if (!author.hasBooks()) {
+				deleteAuthor(author);
+			}
+		}
+	}
+
+	private void deleteAuthor(Author author) {
+		try {
+			String deleteQuery = "delete from tbl_author where authorId=?";
+			PreparedStatement pstmt = conn.prepareStatement(deleteQuery);
+			pstmt.setInt(1, author.getAuthorId());
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	private void findAuthors() {
+		try {
+			String selectQuery = "SELECT * FROM (tbl_author NATURAL JOIN tbl_book_authors) where bookId=?";
+
+			PreparedStatement pstmt = conn.prepareStatement(selectQuery);
+			pstmt.setInt(1, book.getBookId());
+
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				int authorId = rs.getInt("authorId");
+				String authorName = rs.getString("authorName");
+
+				Author author = new Author(authorId, authorName);
+
+				authors.add(author);
+
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void updateBookInDatabase() {
+		System.out.println("enter new book title");
+		String title = sc.nextLine();
+
+		System.out.println("enter author name");
+		String authorName = sc.nextLine();
+
+		try {
+			String updateQuery = "update ((tbl_book natural join tbl_book_authors) natural join tbl_author) set title=?, authorName=? where bookId=?";
+
+			PreparedStatement pstmt = conn.prepareStatement(updateQuery);
+
+			pstmt.setString(1, title);
+			pstmt.setString(2, authorName);
+			pstmt.setInt(3, book.getBookId());
+
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void selectBook() {
+		int bookCounter = displayBooks(libraryBranch.getBranchId());
+
+		int bookChoice = Integer.parseInt(sc.nextLine());
+
+		if (bookCounter == bookChoice) {
+			handleBookAndAuthor();
+			return;
+		}
+
+		updateBookInfo(bookChoice);
+	}
+
+	private void selectLibrary() {
+		int libraryCounter = displayLibraries();
+
+		int libraryChoice = Integer.parseInt(sc.nextLine());
+
+		if (libraryChoice == libraryCounter) {
+			handleBookAndAuthor();
+			return;
+		}
+
+		updateLibraryInfo(libraryChoice);
+
 	}
 
 	private void updateBookInfo(int bookChoice) {
