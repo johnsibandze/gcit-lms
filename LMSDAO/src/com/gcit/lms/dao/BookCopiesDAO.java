9af144ae14924f10071bbe5d5b4b99fd78f37fc4
johnsibandze <johnsibandze@lclark.edu>
2015-07-05 01:47:46 -0400

    Added Librarian Service implementation

---
+++ LMSDAO/src/com/gcit/lms/dao/BookCopiesDAO.java
@@ -0,0 +1,80 @@
+package com.gcit.lms.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.gcit.lms.domain.Author;
+import com.gcit.lms.domain.Book;
+import com.gcit.lms.domain.BookCopies;
+import com.gcit.lms.domain.Genre;
+
+public class BookCopiesDAO extends BaseDAO<BookCopies> {
+
+	public BookCopiesDAO(Connection conn) throws Exception {
+		super(conn);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void create(BookCopies bookCopies) throws Exception {
+		// save the book copy record and get the id
+		int bookId = saveWithID("insert into tbl_book_copies values(?, ?, ?)",
+				new Object[] { bookCopies.getBookId(), bookCopies.getBookId(),
+						bookCopies.getNoOfCopies() });
+
+		// if the book copy wasn't saved
+		if (bookId == -1) {
+			throw new Exception("inserting book copies failed");
+		}
+
+	}
+
+	public void update(BookCopies bookCopies) throws Exception {
+		save("update tbl_book_copies set noOfCopies = ? where bookId = ? and branchId = ?",
+				new Object[] { bookCopies.getNoOfCopies(),
+						bookCopies.getBookId(), bookCopies.getBranchId() });
+	}
+
+	public void delete(BookCopies bookCopies) throws Exception {
+		save("delete from tbl_book_copies where bookId = ? and branchId = ?",
+				new Object[] { bookCopies.getBookId(), bookCopies.getBranchId() });
+	}
+
+	public List<BookCopies> readAll() throws Exception {
+		return (List<BookCopies>) read("select * from tbl_book_copies", null);
+	}
+
+	@Override
+	public List<BookCopies> extractData(ResultSet rs) throws Exception {
+		List<BookCopies> bookCopies = new ArrayList<BookCopies>();
+
+		while (rs.next()) {
+			BookCopies bc = new BookCopies();
+			bc.setBookId(rs.getInt("bookId"));
+			bc.setBranchId(rs.getInt("branchId"));
+			bc.setNoOfCopies(rs.getInt("noOfCopies"));
+
+			bookCopies.add(bc);
+		}
+
+		return bookCopies;
+	}
+
+	@Override
+	public List<BookCopies> extractDataFirstLevel(ResultSet rs)
+			throws Exception {
+		List<BookCopies> books = new ArrayList<BookCopies>();
+
+		while (rs.next()) {
+			BookCopies bc = new BookCopies();
+			bc.setBookId(rs.getInt("bookId"));
+			bc.setBranchId(rs.getInt("branchId"));
+			bc.setNoOfCopies(rs.getInt("noOfCopies"));
+
+			books.add(bc);
+		}
+
+		return books;
+	}
+}
---
+++ LMSDAO/src/com/gcit/lms/service/LibrarianService.java
@@ -0,0 +1,60 @@
+package com.gcit.lms.service;
+
+import java.sql.Connection;
+
+import com.gcit.lms.dao.BookCopiesDAO;
+import com.gcit.lms.dao.LibraryBranchDAO;
+import com.gcit.lms.domain.BookCopies;
+import com.gcit.lms.domain.LibraryBranch;
+
+public class LibrarianService {
+
+	public void updateLibraryBranch(LibraryBranch libraryBranch)
+			throws Exception {
+
+		Connection conn = ConnectionUtil.createConnection();
+		try {
+			if (libraryBranch == null || libraryBranch.getBranchName() == null
+					|| libraryBranch.getBranchName().length() == 0
+					|| libraryBranch.getBranchName().length() > 45) {
+				throw new Exception(
+						"Library Branch Name cannot be empty or more than 45 Chars");
+			} else if (libraryBranch == null
+					|| libraryBranch.getBranchAddress() == null
+					|| libraryBranch.getBranchAddress().length() == 0
+					|| libraryBranch.getBranchAddress().length() > 45) {
+				throw new Exception(
+						"Library Branch Address cannot be empty or more than 45 Chars");
+			} else {
+				LibraryBranchDAO lbdao = new LibraryBranchDAO(conn);
+				lbdao.update(libraryBranch);
+				conn.commit();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			conn.rollback();
+		} finally {
+			conn.close();
+		}
+	}
+
+	public void addBookCopies(BookCopies bookCopies) throws Exception {
+
+		Connection conn = ConnectionUtil.createConnection();
+		try {
+			if (bookCopies == null || bookCopies.getNoOfCopies() == 0) {
+				throw new Exception("You must add at least 1 copy");
+			} else {
+				BookCopiesDAO bcdao = new BookCopiesDAO(conn);
+				bcdao.create(bookCopies);
+				conn.commit();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			conn.rollback();
+		} finally {
+			conn.close();
+		}
+	}
+
+}
